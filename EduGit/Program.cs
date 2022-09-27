c39afd0cb392edd33930f0f5d3405d7fa00227c3
Owner <Owner@SEXY>
2022-09-27 09:24:53 +0200

    Add project files.

---
+++ EduGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EduGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
